93df17191fa06f73043953bb94fecf0cb2f37cef
Ashwani_Arora01 <Ashwani_Arora01@punitp113175l.ad.infosys.com>
2015-03-08 23:39:17 -0400

    Initial Commit

---
+++ JavaServerFaces/src/com/mkyong/common/HelloBean.java
@@ -0,0 +1,31 @@
+package com.mkyong.common;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable {
+
+	private static int  count=0;
+	public void init()
+	{
+		count++;
+		System.out.println("inside Hello Bean "+count);
+	}
+	
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
